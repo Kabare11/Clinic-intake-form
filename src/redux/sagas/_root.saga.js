b0a97c567cca985a7d4451520418e5aa8f27e8fd
Abdulahi Kabare <abdullahikabare@gmail.com>
2024-02-13 00:45:04 -0600

    Registered appointment and emergency sagas

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -4,6 +4,7 @@ import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import profileSaga from './patient_profile.saga';
 import appointmentSaga from './appointment.saga';
+import emergencySaga from './emergency.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -18,6 +19,7 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     profileSaga(),
-    appointmentSaga()
+    appointmentSaga(),
+    emergencySaga()
   ]);
 }
