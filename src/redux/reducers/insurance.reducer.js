8dff390cc92c1cac260d459254e8f6d77e11d85a
Abdulahi Kabare <abdullahikabare@gmail.com>
2024-02-13 10:10:47 -0600

    Created Insurance Reducer and Registered it in The Root Reducer

---
+++ src/redux/reducers/insurance.reducer.js
@@ -0,0 +1,16 @@
+
+const insurance = (state = {}, action) => {
+   switch (action.type) {
+      case 'SET_INSURANCE':
+         return action.payload;
+      case 'UNSET_USER':
+         return {};
+      default:
+         return state;
+   }
+
+};
+
+// user will be on the redux state at:
+// state.user
+export default insurance;
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -5,6 +5,7 @@ import userSaga from './user.saga';
 import profileSaga from './patient_profile.saga';
 import appointmentSaga from './appointment.saga';
 import emergencySaga from './emergency.saga';
+import insuranceSaga from './insurance.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -20,6 +21,7 @@ export default function* rootSaga() {
     userSaga(),
     profileSaga(),
     appointmentSaga(),
-    emergencySaga()
+    emergencySaga(),
+    insuranceSaga()
   ]);
 }
